7a6317db20a559c1e597fde710d111364a5b50c9
Pozsár Bence Zsolt <bencepozsi@gmail.com>
2024-11-15 15:53:07 +0100

    compose file written with an initial backend project, frontend container needed

---
+++ backend/src/main/java/com/dartsfighters/advancedjava/DartsFightersApplication.java
@@ -0,0 +1,13 @@
+package com.dartsfighters.advancedjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartsFightersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartsFightersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dartsfighters/advancedjava/DartsFightersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dartsfighters.advancedjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsFightersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
